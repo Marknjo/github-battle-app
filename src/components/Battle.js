30a443edf0afc04d6adb2c973a0e6d0e421d46ba
Mark Njoroge <marknjo7@gmail.com>
2020-08-09 15:38:11 +0300

    Cleaned unused properties in the componet

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -6,8 +6,7 @@ import {
 	FaTimesCircle,
 } from "react-icons/fa";
 import PropTypes from "prop-types";
-import Results from "./Results";
-import ThemeContext, { ThemeConsumer } from "../contexts/theme";
+import ThemeContext from "../contexts/theme";
 import { Link } from "react-router-dom";
 
 /**
