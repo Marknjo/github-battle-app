7a8408eafa0e60195c4e5cb3883c02481996bdbc
Mark Njoroge <marknjo7@gmail.com>
2020-08-11 13:42:06 +0300

    Refactored the results component to a React Hook Component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,118 +1,153 @@
-import React from 'react'
-import { battle } from '../utils/api'
-import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
-import Card from './Card'
-import PropTypes from 'prop-types'
-import Loading from './Loading'
-import Tooltip from './Tooltip'
-import queryString from 'query-string'
-import { Link } from 'react-router-dom'
+import React, { useReducer, useEffect } from "react";
+import { battle } from "../utils/api";
+import {
+	FaCompass,
+	FaBriefcase,
+	FaUsers,
+	FaUserFriends,
+	FaUser,
+} from "react-icons/fa";
+import Card from "./Card";
+import PropTypes from "prop-types";
+import Loading from "./Loading";
+import Tooltip from "./Tooltip";
+import queryString from "query-string";
+import { Link } from "react-router-dom";
 
-function ProfileList ({ profile }) {
-  return (
-    <ul className='card-list'>
-      <li>
-        <FaUser color='rgb(239, 115, 115)' size={22} />
-        {profile.name}
-      </li>
-      {profile.location && (
-        <li>
-          <Tooltip text="User's location">
-            <FaCompass color='rgb(144, 115, 255)' size={22} />
-            {profile.location}
-          </Tooltip>
-        </li>
-      )}
-      {profile.company && (
-        <li>
-          <Tooltip text="User's company">
-            <FaBriefcase color='#795548' size={22} />
-            {profile.company}
-          </Tooltip>
-        </li>
-      )}
-      <li>
-        <FaUsers color='rgb(129, 195, 245)' size={22} />
-        {profile.followers.toLocaleString()} followers
-      </li>
-      <li>
-        <FaUserFriends color='rgb(64, 183, 95)' size={22} />
-        {profile.following.toLocaleString()} following
-      </li>
-    </ul>
-  )
+function ProfileList({ profile }) {
+	return (
+		<ul className="card-list">
+			<li>
+				<FaUser color="rgb(239, 115, 115)" size={22} />
+				{profile.name}
+			</li>
+			{profile.location && (
+				<li>
+					<Tooltip text="User's location">
+						<FaCompass color="rgb(144, 115, 255)" size={22} />
+						{profile.location}
+					</Tooltip>
+				</li>
+			)}
+			{profile.company && (
+				<li>
+					<Tooltip text="User's company">
+						<FaBriefcase color="#795548" size={22} />
+						{profile.company}
+					</Tooltip>
+				</li>
+			)}
+			<li>
+				<FaUsers color="rgb(129, 195, 245)" size={22} />
+				{profile.followers.toLocaleString()} followers
+			</li>
+			<li>
+				<FaUserFriends color="rgb(64, 183, 95)" size={22} />
+				{profile.following.toLocaleString()} following
+			</li>
+		</ul>
+	);
 }
 
 ProfileList.propTypes = {
-  profile: PropTypes.object.isRequired,
-}
+	profile: PropTypes.object.isRequired,
+};
 
-export default class Results extends React.Component {
-  state = {
-    winner: null,
-    loser: null,
-    error: null,
-    loading: true
-  }
-  componentDidMount () {
-    const { playerOne, playerTwo } = queryString.parse(this.props.location.search)
+const resultsReducer = (state, action) => {
+	switch (action.type) {
+		case "BATTLE_RESULTS":
+			return {
+				...state,
+				error: null,
+				loading: false,
+				winner: action.payload.winner,
+				loser: action.payload.loser,
+			};
 
-    battle([ playerOne, playerTwo ])
-      .then((players) => {
-        this.setState({
-          winner: players[0],
-          loser: players[1],
-          error: null,
-          loading: false
-        })
-      }).catch(({ message }) => {
-        this.setState({
-          error: message,
-          loading: false
-        })
-      })
-  }
-  render() {
-    const { winner, loser, error, loading } = this.state
+		case "BATTLE_ERROR":
+			return {
+				...state,
+				loading: false,
+				error: action.payload.error,
+			};
 
-    if (loading === true) {
-      return <Loading text='Battling' />
-    }
+		default:
+			throw new Error("System could not recogonize the type of action given.");
+	}
+};
 
-    if (error) {
-      return (
-        <p className='center-text error'>{error}</p>
-      )
-    }
+const resultsInitialtState = {
+	winner: null,
+	loser: null,
+	error: null,
+	loading: true,
+};
+const Results = ({ location }) => {
+	const [state, dispatch] = useReducer(resultsReducer, resultsInitialtState);
 
-    return (
-      <React.Fragment>
-        <div className='grid space-around container-sm'>
-          <Card
-            header={winner.score === loser.score ? 'Tie' : 'Winner'}
-            subheader={`Score: ${winner.score.toLocaleString()}`}
-            avatar={winner.profile.avatar_url}
-            href={winner.profile.html_url}
-            name={winner.profile.login}
-          >
-            <ProfileList profile={winner.profile}/>
-          </Card>
-          <Card
-            header={winner.score === loser.score ? 'Tie' : 'Loser'}
-            subheader={`Score: ${loser.score.toLocaleString()}`}
-            avatar={loser.profile.avatar_url}
-            name={loser.profile.login}
-            href={loser.profile.html_url}
-          >
-            <ProfileList profile={loser.profile}/>
-          </Card>
-        </div>
-        <Link
-          to='/battle'
-          className='btn dark-btn btn-space'>
-            Reset
-        </Link>
-      </React.Fragment>
-    )
-  }
-}
+	const { winner, loser, error, loading } = state;
+
+	useEffect(() => {
+		const { playerOne, playerTwo } = queryString.parse(location.search);
+		let isCurrent = true;
+
+		battle([playerOne, playerTwo])
+			.then((players) => {
+				if (isCurrent) {
+					dispatch({
+						type: "BATTLE_RESULTS",
+						payload: {
+							winner: players[0],
+							loser: players[1],
+						},
+					});
+				}
+			})
+			.catch(({ message }) => {
+				dispatch({
+					type: "BATTLE_ERROR",
+					error: message,
+				});
+			});
+
+		return () => (isCurrent = false);
+	}, [location]);
+
+	if (loading === true) {
+		return <Loading text="Battling" />;
+	}
+
+	if (error) {
+		return <p className="center-text error">{error}</p>;
+	}
+
+	return (
+		<>
+			<div className="grid space-around container-sm">
+				<Card
+					header={winner.score === loser.score ? "Tie" : "Winner"}
+					subheader={`Score: ${winner.score.toLocaleString()}`}
+					avatar={winner.profile.avatar_url}
+					href={winner.profile.html_url}
+					name={winner.profile.login}
+				>
+					<ProfileList profile={winner.profile} />
+				</Card>
+				<Card
+					header={winner.score === loser.score ? "Tie" : "Loser"}
+					subheader={`Score: ${loser.score.toLocaleString()}`}
+					avatar={loser.profile.avatar_url}
+					name={loser.profile.login}
+					href={loser.profile.html_url}
+				>
+					<ProfileList profile={loser.profile} />
+				</Card>
+			</div>
+			<Link to="/battle" className="btn dark-btn btn-space">
+				Reset
+			</Link>
+		</>
+	);
+};
+
+export default Results;
